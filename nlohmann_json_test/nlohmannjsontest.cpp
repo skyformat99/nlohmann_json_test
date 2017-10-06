a5bcd4fbfd2ccc287c86a629e5c3211f2aad8292
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2017-10-06 15:07:29 +0700

    Newly created hello world console app.

---
+++ nlohmann_json_test/nlohmannjsontest.cpp
@@ -0,0 +1,11 @@
+// nlohmannjsontest.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
